d9ee00f4b4e556b6f5c5cffd674caed5cff00348
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-02-26 20:20:07 -0600

    adds react add and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow, mount } from 'enzyme';
+import Counter from "./Counter"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Counter Testing", () => {
+
+  let wrapper;
+
+  // Jest - sets up common settings in all tests
+  beforeEach(() => {
+    // shallow does not render children components.
+    wrapper = shallow(<Counter />)
+
+    // mount extends DOM tree
+    // NOT compatible with React 17 - https://github.com/enzymejs/enzyme/issues/2462
+    // wrapper = mount(<App />)
+    console.log(wrapper.debug())
+  })
+
+  test('render the title of counter', () => {
+
+    // .debug() logs the JSX
+    // console.log(wrapper.debug())
+
+    // .find() - finds based on tag, class, attribute, etc.
+    // .text() - looks for text.
+    // .toContain() - match data or regex.
+    expect(wrapper.find("h1").text()).toContain("This is counter app");
+
+    // DEFAULT Test Code
+    // render(<App />);
+    // const linkElement = screen.getByText(/Learn React/i);
+    // expect(linkElement).toBeInTheDocument();
+  });
+
+  test("Render button with text 'increment'", () => {
+    expect(wrapper.find('#increment-btn').text()).toBe("Increment")
+  })
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  })
+
+  test("render the click event of increment button and increment counter value", () => {
+    // Enzyme - .simulate()
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  })
+
+  test("render the click event of decrement button and decrement counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  })
+
+})
+
+
